1f056c573c483edbba4bd5b037b08de1e708d509
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-20 13:14:09 -0700

    Created 'SinglePuppy.jsx' component, to be used for 'onclick'; import&mount to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import PuppyList from './PuppyList.jsx'
+import SinglePuppy from './SinglePuppy.jsx'
 
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
   return (
     <>
     <h1>Hello World!</h1>
-    < PuppyList />
+    <PuppyList />
+    <SinglePuppy />
     </>
   )
 }
---
+++ src/SinglePuppy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SinglePuppy(){
+    return(
+        <>
+        <h2>What's up?!</h2>
+        </>
+    )
+}
+
+export default SinglePuppy;
