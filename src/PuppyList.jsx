0b76eccdcbf1b20086bc2a133decd1e19ac8c033
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-20 13:45:23 -0700

    useState/useEffect.

--- src/PuppyList.jsx
+++ src/PuppyList.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 function PuppyList(){
+//useState
+const [puppies, setPuppies] = useState([]);
 
-    //FETCH 'response' 'https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players'
-// useEffect(() => {
+useEffect(() => {
     const getPuppyList = async() => {
         const response = await fetch `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players`;
-        //FETCH 'responseJson'
         const responseJson = await response.json();
         const listOfPuppyPlayers = responseJson.data.players;
         console.log(listOfPuppyPlayers);
-
-    // var = responseJson.results
-    // setState(var)
+        setPuppies(listOfPuppyPlayers);
 
     }
     getPuppyList();
+}, []);
 
 
     return(
         <>
         <h2>Hi there.</h2>
+        {
+            puppies.map((singlePuppy) => {
+                return <li>{singlePuppy.name}</li>
+            })
+        }
         </>
     )
 }
