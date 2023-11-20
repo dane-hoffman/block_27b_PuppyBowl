1ae92d8b6b2919aacf1927f307f30cdbcc8791e4
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-20 13:27:25 -0700

    getPuppyList async function added inside PuppyList to fetch response.

--- src/PuppyList.jsx
+++ src/PuppyList.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 function PuppyList(){
+
     //FETCH 'response' 'https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players'
+// useEffect(() => {
+    const getPuppyList = async() => {
+        const response = await fetch `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players`;
+        console.log(response);
     //FETCH 'responseJson'
     // var = responseJson.results
     // setState(var)
-    // PuppyList();
+
+    }
+    getPuppyList();
+
 
     return(
         <>
