85f6609f475f7181c1e6f740dd92a2cbb95bd6e5
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-20 13:18:57 -0700

    Pseudocode next steps for API.

--- src/PuppyList.jsx
+++ src/PuppyList.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 function PuppyList(){
+    //FETCH 'response' 'https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players'
+    //FETCH 'responseJson'
+    // var = responseJson.results
+    // setState(var)
+    // PuppyList();
+
     return(
         <>
         <h2>Hi there.</h2>
